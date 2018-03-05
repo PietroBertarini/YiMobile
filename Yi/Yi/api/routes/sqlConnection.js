d83cc2f3c0962e164ab1bafab04b10ae7a41d74f
Pietro Bertarini <Pietro Bertarini>
2018-03-04 21:51:35 -0300

    first commit,data base+sql funcionando.post testado

---
+++ Yi/Yi/api/routes/sqlConnection.js
@@ -0,0 +1,18 @@
+﻿var mysql = require('mysql');
+
+var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'senha',
+    port: '3000',
+    database: 'yidt'
+});
+connection.connect((err) => {
+    if (err) {
+        throw err;
+    }
+    console.log('my sql connected');
+
+});
+
+module.exports = connection;
---
+++ Yi/Yi/api/routes/user.js
@@ -0,0 +1,1 @@
+﻿const mySql = require('mysql');
