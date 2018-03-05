625343c001921a270560fd00e350d29a139d6851
Pietro Bertarini <Pietro Bertarini>
2018-03-05 11:33:48 -0300

    asserts definidos

--- Yi/Yi/api/routes/usuarioRoute.js
+++ Yi/Yi/api/routes/usuarioRoute.js
@@ -13,31 +13,36 @@ router.get('/', (req, res, next) => {
 
 
 router.post('/signup', (req, res, next) => {
-    bcrypt.hash(req.body.senha, 10, (err, hash) => {
-           if (err) {
-             return res.status(500).json({
-                error: err
-           });
-        } else {
-           const usuario = {
-             email: req.body.email,
-            senha: hash
-               };
-               
-            var query = mysql.query('INSERT INTO usuario SET ?', usuario, function (err, result) {
-               console.log(query.sql);
-              if (err) {
-                 throw err;
-            }
-         });
-         }
-          res.status(201).json({
-             message: 'user created',
-         //   novoUsuario: usuario
+    if (req.body.email != undefined && req.body.senha != undefined) {
+        bcrypt.hash(req.body.senha, 10, (err, hash) => {
+            if (err) {
+                return res.status(500).json({
+                    error: err
+                });
+            } else {
+                const usuario = {
+                    email: req.body.email,
+                    senha: hash
+                };
+
+                var query = mysql.query('INSERT INTO usuario SET ?', usuario, function (err, result) {
+                    console.log(query.sql);
+                    if (err) {
+                        throw err;
+                    }
+                });
+                res.status(201).json({
+                    message: 'user created',
+                    //   novoUsuario: usuario
 
                 });
 
-
-    });
+            }
+        });
+     }else {
+        res.status(400).json({
+            message: 'campo vazio',
+        });
+    } 
 });
 module.exports = router;
