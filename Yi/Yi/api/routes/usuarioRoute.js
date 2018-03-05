90549e31adf1a5d5d4c7ec9eec9d784901a3f7da
Pietro Bertarini <Pietro Bertarini>
2018-03-05 16:07:42 -0300

    save point

--- Yi/Yi/api/routes/usuarioRoute.js
+++ Yi/Yi/api/routes/usuarioRoute.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const mysql = require('./sqlConnection');
 const User = require("../models/usuarioModel");
 const bcrypt = require("bcrypt");
+//const jwt = require("jsonwebtoken");
 
 
 
@@ -10,6 +11,59 @@ router.get('/', (req, res, next) => {
     res.status(200).json({
         message: 'usuario get'
     })
+});
+router.post('/login', (req, res, next) => {
+    if (req.body.email != undefined && req.body.senha != undefined) {
+        var querySelect = mysql.query("SELECT * FROM usuario WHERE email= '" + req.body.email + "'", function (err, result) {
+            if (result[0] == undefined) {
+                if (err) {
+                    throw err;
+                }
+                else {
+                    res.status(400).json({
+                        message: 'usuario nao existente'
+                    });
+                }
+            }
+            else {
+                bcrypt.compare(req.body.senha, result[0].senha,(err, ress)=>{
+            if (err) {
+                return ress.status(401).json({
+                    message: 'Problem At Login' 
+                });
+            }
+            if (ress) {
+               // const token=jwt.sign({
+                //    email: result[0].email,
+                 //   senha: result[0].senha
+
+ //               }, secret,
+//                {
+ //                   expiresIn: "1h"
+  //              })
+                return res.status(200).json({
+                    message: 'Login Sucess',
+                    tokenLogIn: token
+                });
+
+            }
+            res.status(401).json({
+                message: 'Senha incorreta'
+
+            });
+
+        });
+              //  res.status(400).json({
+               //     message: 'usuario existente'
+               // });
+
+            }
+        });
+    } else {
+        res.status(400).json({
+            message: 'campo vazio',
+        });
+    }
 });
 
 
