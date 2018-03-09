805fc9548772621864e1ca84b5dc3fb68ffa735b
Pietro Bertarini <Pietro Bertarini>
2018-03-09 15:48:43 -0300

    Retirado comentario

--- Yi/Yi/server.js
+++ Yi/Yi/server.js
@@ -7,9 +7,3 @@ const server = http.createServer(app);
 
 
 server.listen(port);
-/*
-http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World\n');
-}).listen(port);
-*/
